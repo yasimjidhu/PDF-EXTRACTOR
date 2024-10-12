621cb0bb8a29dae58b08a7a8107a4ff205c213b8
yasimjidhu <dirshadyasim@gmail.com>
2024-10-12 18:57:19 +0530

    vite config file updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,23 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          if (id.includes('pdfjs-dist')) {
-            return 'pdfjs';
-          }
-        }
-      }
-    }
+        manualChunks: {
+          pdfjs: ['pdfjs-dist/build/pdf'],
+        },
+      },
+    },
   },
-  resolve: {
-    alias: {
-      'pdfjs-dist': path.resolve(__dirname, 'node_modules/pdfjs-dist'),
-    }
-  }
 });
